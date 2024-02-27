870d4b1dcc46733b06acad2ea8c68798ac5d4391
ClaraCPC <clara.palacios.spain@gmail.com>
2024-02-27 22:39:56 +0100

    final

--- titulos.js
+++ titulos.js
@@ -4,6 +4,9 @@ const searchBar = document.getElementById('search');
 const selector = document.createElement('select');
 selector.id = 'titulo';
 
+const book_year = document.getElementById('book_year');
+const book_author = document.getElementById('book_author');
+
 searchBar.addEventListener('keyup', function (event) {
   const tituloBuscado = event.target.value;
   if (tituloBuscado.length > 2) {
@@ -15,7 +18,8 @@ searchBar.addEventListener('keyup', function (event) {
 });
 
 function busqueda(tituloBuscado) {
-  fetch(`https://openlibrary.org/search.json?q=${tituloBuscado}`)
+  tituloBuscado = tituloBuscado.replace(/ /g, "+");
+  fetch(`https://openlibrary.org/search.json?title=${tituloBuscado}`)
     .then(response => {
       if (!response.ok) {
         throw new Error('Network response was not ok');
@@ -25,12 +29,19 @@ function busqueda(tituloBuscado) {
     .then(data => {
       const docs = data.docs;
       selector.innerHTML = ''; // Clear the select options
+      book_year.innerHTML = '';
       docs.forEach(book => {
         const option = document.createElement('option');
         option.value = book.title;
         option.textContent = book.title;
         selector.appendChild(option);
       });
+      selector.addEventListener('change', function(event) {
+        const libro_seleccionado = event.target.value;
+        busqueda_year(libro_seleccionado);
+        busqueda_autor(libro_seleccionado);
+        //book_year.innerHTML = 'Año de publicación: ' + year;
+    });
       const divLista = document.getElementById('divLista');
       if (!divLista) {
         const container = document.getElementById('books');
@@ -47,4 +58,43 @@ function busqueda(tituloBuscado) {
     .catch(error => {
       console.error('There was a problem with your fetch operation:', error);
     });
+}
+
+
+function busqueda_year(titulo){
+  titulo = titulo.replace(/ /g, "+");
+  fetch(`https://openlibrary.org/search.json?title=${titulo}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      const year = data.docs[0].first_publish_year;
+      console.log(year);
+      book_year.innerHTML = 'Año de publicación: ' + year;
+    })
+    .catch(error => {
+      console.error('There was a problem with your fetch operation:', error);
+    });  
+}
+
+function busqueda_autor(titulo){
+  titulo = titulo.replace(/ /g, "+");
+  fetch(`https://openlibrary.org/search.json?title=${titulo}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      const author = data.docs[0].author_name[1];
+      book_author.innerHTML = 'Autor: ' + author;
+      console.log(author);
+    })
+    .catch(error => {
+      console.error('There was a problem with your fetch operation:', error);
+    });  
 }
